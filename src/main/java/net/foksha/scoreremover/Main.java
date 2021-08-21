7dc717cc6104eb287602d7a1a5a7e73c35e73462
Foksha! <55259471+FokshaWasTaken@users.noreply.github.com>
2021-08-21 23:35:13 +0200

    only tested in dev environment lol

---
+++ src/main/java/net/foksha/scoreremover/Main.java
@@ -0,0 +1,11 @@
+package net.foksha.scoreremover;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+		System.out.println("Score? Nowhere.");
+	}
+}
---
+++ src/main/java/net/foksha/scoreremover/mixin/MixinDeathScreen.java
@@ -0,0 +1,34 @@
+package net.foksha.scoreremover.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.block.AirBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.client.gui.screen.DeathScreen;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.ShulkerBoxSlot;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Direction;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DeathScreen.class)
+public abstract class MixinDeathScreen
+{
+	@Shadow private Text scoreText;
+
+	@Redirect(method = "init", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/DeathScreen;scoreText:Lnet/minecraft/text/Text;", opcode = Opcodes.PUTFIELD))
+	private void doNotSetScoreText(DeathScreen deathScreen, Text value) {
+		this.scoreText = Text.of("");
+	}
+}
